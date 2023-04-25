f5a2d5a3e20a7feb24f4936e7eb6c1b713077899
cjmin1233 <cjmin1233@gmail.com>
2023-04-25 17:34:17 +0900

    Drag & Shoot
    화면 드래그시 line renderer로 길이 표시
    드래그 릴리스시 플레이어 발사

--- 3D_demo/Assets/Scripts/PlayerController.cs
+++ 3D_demo/Assets/Scripts/PlayerController.cs
@@ -2,26 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    [SerializeField] private TextMeshProUGUI test_text;
-    Vector2 diff_vec;
-    [SerializeField] private float speed;
+    private LineRenderer lr;
+
+    [SerializeField] private float power = 5f;
+    [SerializeField] private float maxDrag = 5f;
+
+    Vector3 dragStartPos;
+    Vector3 draggingPos;
+    Vector3 dragReleasePos;
+    Touch touch;
+    GameObject player;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        lr = GetComponent<LineRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
     private void Update()
     {
-        if (Input.touchCount == 1)
+        if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began) diff_vec = touch.deltaPosition;
-            else if (touch.phase == TouchPhase.Ended) diff_vec = touch.position - diff_vec;
-            test_text.text = diff_vec.ToString();
-            rb.velocity = new Vector3(diff_vec.x, 0f, diff_vec.y) * speed;
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                DragStart();
+            }
+            if (touch.phase == TouchPhase.Moved || touch.phase==TouchPhase.Stationary)
+            {
+                Dragging();
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                DragRelease();
+            }
         }
     }
+
+    private void DragStart()
+    {
+        dragStartPos = touch.position;
+        dragStartPos.z = dragStartPos.y;
+        dragStartPos.y = 0f;
+        lr.positionCount = 1;
+        lr.SetPosition(0, player.transform.position);
+    }
+
+    private void Dragging()
+    {
+        draggingPos = touch.position;
+        draggingPos.z = draggingPos.y;
+        draggingPos.y = 0f;
+        Vector3 diff_vec = draggingPos - dragStartPos;
+        print(diff_vec);
+        diff_vec.x /= Screen.width / 2;
+        diff_vec.z /= Screen.height / 2;
+        lr.positionCount = 2;
+        lr.SetPosition(0, player.transform.position);
+        lr.SetPosition(1, player.transform.position + diff_vec);
+    }
+    private void DragRelease()
+    {
+        lr.positionCount = 0;
+
+        Vector3 dragReleasePos = touch.position;
+        dragReleasePos.z = dragReleasePos.y;
+        dragReleasePos.y = 0f;
+        Vector3 force = dragReleasePos - dragStartPos;
+        Vector3 clampedForce = Vector3.ClampMagnitude(force, maxDrag) * power;
+        rb.AddForce(clampedForce, ForceMode.Impulse);
+    }
+
 }
